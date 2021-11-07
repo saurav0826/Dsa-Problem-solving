d1fb1d882f66abdfd012e95da76ea975d15cc4d9
Saurav_Shinde <shindesaurav0826@gmail.com>
2021-11-07 15:30:04 +0530

    Majority element

---
+++ array/Majority element.java
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+             arr[i] = sc.nextInt();
+             
+           System.out.println(new Solution().majorityElement(arr, n)); 
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int majorityElement(int arr[], int n)
+    {
+        // your code here
+         int maxCount = 0;
+        int index = -1; // sentinels
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[i] == arr[j])
+                    count++;
+            }
+
+            // update maxCount if count of
+            // current element is greater
+            if (count > maxCount) {
+                maxCount = count;
+                index = i;
+            }
+        }
+
+        // if maxCount is greater than n/2
+        // return the corresponding element
+        if (maxCount > n / 2)
+            return(arr[index]);
+
+        else
+            return -1;
+    }
+}
