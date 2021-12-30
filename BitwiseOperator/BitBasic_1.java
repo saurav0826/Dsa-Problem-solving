c846fb858452aff3fdf35a58f25aaf437a8d2f8d
Saurav_Shinde <shindesaurav0826@gmail.com>
2021-12-31 01:15:28 +0530

    code is added

---
+++ BitwiseOperator/BitBasic_1.java
@@ -0,0 +1,13 @@
+package BitwiseOperator;
+
+public class BitBasic_1 {
+
+public static void main (String[] args) {
+	    
+	    int x = 3, y = 6;
+	    
+		System.out.println(x ^ y);
+		System.out.println(x | y);
+		System.out.println(x & y);
+}
+}
---
+++ BitwiseOperator/BitBasic_2.java
@@ -0,0 +1,28 @@
+package BitwiseOperator;
+
+public class BitBasic_2 {
+	public static void main (String[] args) {
+	    
+	    int o = -1;
+	    
+	    System.out.println(o << 1);
+
+	    
+	    int x = 3;
+	    
+	    System.out.println(x << 1);
+	    
+	    System.out.println(x << 2);
+	    
+	    int y = 4;
+	    
+	    int z = (x << y);
+	    
+	    System.out.println(z);
+	    
+	    int k = 1;
+	    
+		System.out.println(~ k);
+	    
+	}
+}
