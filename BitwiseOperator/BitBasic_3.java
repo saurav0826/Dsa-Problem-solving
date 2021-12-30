538078453dd3004a8e7e21680a6764ba0dde9e73
Saurav_Shinde <shindesaurav0826@gmail.com>
2021-12-31 01:29:45 +0530

    code is added

---
+++ BitwiseOperator/BitBasic_3.java
@@ -0,0 +1,19 @@
+package BitwiseOperator;
+
+public class BitBasic_3 {
+public static void main (String[] args) {
+	    
+	    int x = -2;
+	    
+	    System.out.println(x >>> 1);
+	    System.out.println(x >> 1);
+	    
+int y= 33;
+	    
+	    System.out.println(y >> 2);
+	    System.out.println(y >> 1);
+	    
+	}
+	
+	
+}
