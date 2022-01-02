37e2e4e111babf9149816e82740c35ff5dc2aafb
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-01-02 14:47:02 +0530

    code is added

---
+++ BitwiseOperator/PowerofTwo.java
@@ -0,0 +1,16 @@
+package BitwiseOperator;
+
+public class PowerofTwo {
+	 static boolean isPow2(int n)
+	    {
+	        if(n == 0)
+	            return true;
+	        
+	        return ((n & (n - 1)) == 0);
+	    }
+		public static void main (String[] args) {
+		    
+		    int n = 4;
+			System.out.println(isPow2(n));
+		}
+}
