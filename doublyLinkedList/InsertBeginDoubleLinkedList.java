b52490afe9df0518f45b89ae2c0937ccecfdae2c
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-01-06 03:11:16 +0530

    code is added

---
+++ doublyLinkedList/InsertBeginDoubleLinkedList.java
@@ -0,0 +1,56 @@
+package doublelinkedlist;
+
+
+public class InsertBeginDoubleLinkedList {
+	public static void main (String[] args) {
+		Node head = new Node(10);
+		Node temp1 = new Node(20);
+		Node temp2 = new Node(30);
+		Node temp3= new Node(50);
+		head.prev=null;
+		head.next=temp1;
+		temp1.prev=head;
+		temp1.next=temp2;
+		temp2.prev=temp1;
+		temp2.next=temp3;
+		temp3.prev=temp2;
+	rec_tranv(head);
+	System.out.println("  ");
+	head=insertbeg(70,head);
+	System.out.println("  ");
+	rec_tranv(head);
+	System.out.println("  ");
+	head=insertbeg(1000,head);
+	rec_tranv(head);
+	
+}
+
+  static Node insertbeg(int i , Node head) {
+		// TODO Auto-generated method stub
+      Node added = new Node(i);
+      added.next=head;
+      if(head!=null)
+    	  head.prev=added;
+      return added;
+	}
+
+static void rec_tranv(Node head) {
+	// TODO Auto-generated method stub
+	if(head==null) {
+		return;
+	}
+	System.out.println(head.data);
+	rec_tranv(head.next);
+	}
+
+static class Node{
+	int data;
+	Node next;
+	Node prev;
+	Node(int data){
+		this.data=data;
+		next=null;
+	}
+	
+}
+}
