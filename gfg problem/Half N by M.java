298c5a4ef59b2c9ca9518adeea447a4768790190
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-01-10 01:55:00 +0530

    code is added

---
+++ gfg problem/Half N by M.java
@@ -0,0 +1,38 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String a[] = in.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(a[0]);
+            int M = Integer.parseInt(a[1]);
+            
+            Solution ob = new Solution();
+            System.out.println(ob.mthHalf(N, M));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int mthHalf(int N, int M){
+        // code here
+        while(M!=1){
+            N=N/2;
+            M--;
+            if(N==0)
+            return 0;
+        }
+        return N;
+    }
+}
