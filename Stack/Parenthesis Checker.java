583b6ca013bae23bd9e86080ba462523e9cfd5a2
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-01-12 18:50:26 +0530

    code is added

---
+++ Stack/Parenthesis Checker.java
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        //Reading total number of testcases
+        int t= sc.nextInt();
+        
+        while(t-- >0)
+        {
+            //reading the string
+            String st = sc.next();
+            
+            //calling ispar method of Paranthesis class 
+            //and printing "balanced" if it returns true
+            //else printing "not balanced"
+            if(new Solution().ispar(st) == true)
+                System.out.println("balanced");
+            else
+                System.out.println("not balanced");
+        
+        }
+    }
+}// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to check if brackets are balanced or not.
+    static boolean ispar(String x)
+    {
+        // add your code here
+        ArrayDeque<Character> s = new ArrayDeque<>();
+        
+        for(int i = 0 ; i < x.length() ; i++){
+            if(x.charAt(i)=='{'||x.charAt(i)=='('||x.charAt(i)=='['){
+                s.push(x.charAt(i));
+            }
+            else{
+                if(s.isEmpty()==true)
+                return false;
+               else if(x.charAt(i)=='}'&&s.peek()=='{'||x.charAt(i)==']'&&s.peek()=='['||x.charAt(i)==')'&&s.peek()=='(')
+                s.pop();
+                else 
+                return false;
+            }
+        }
+        
+        return s.isEmpty()==true;
+    }
+}
