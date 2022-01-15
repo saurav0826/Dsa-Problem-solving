bee5584280ff54d6ab0ce013a66297e00294c82b
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-01-15 22:47:42 +0530

    code is added

---
+++ deque/Basic_2.java
@@ -0,0 +1,34 @@
+package deque;
+
+import java.util.*;
+
+public class Basic_2 {
+	  public static void main(String[] args) 
+	    {
+	        Deque<Integer> d = new LinkedList<Integer>();
+	        
+	        // Adds element to front of queue
+	        d.addFirst(10);
+	        
+	        // Adds element to end of queue
+	        d.addLast(20);
+	        
+	        // Adds element to front of queue
+	        d.addFirst(5);
+	        
+	        // Adds element to end of queue
+	        d.addLast(15);
+	        
+	        // Traversal using Iterator
+	        Iterator it = d.iterator();
+	        while(it.hasNext())
+	            System.out.print(it.next() + " ");
+	        System.out.println();
+	        
+	        // Traversal using for-each 
+	        for(int x: d)
+	            System.out.print(x + " ");
+	        System.out.println();
+
+	    }
+	}
