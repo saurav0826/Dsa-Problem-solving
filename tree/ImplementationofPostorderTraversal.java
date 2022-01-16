4be88ecbc72ffa693beda9a99cc95734266b07fd
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-01-17 00:11:59 +0530

    code is added

---
+++ tree/ImplementationofPostorderTraversal.java
@@ -0,0 +1,38 @@
+package tree;
+
+
+public class ImplementationofPostorderTraversal {
+
+	static class Node{
+		 int key ;
+		 Node left;
+		 Node right;
+		 Node(int i ){
+			 key=i;
+		 }
+		 public static void main(String [] args) {
+			 Node root = new Node (30);
+			 root.left= new Node (25);
+			 root.left.left= new Node (10);
+			 root.left.right= new Node (20);
+		     root.right= new Node (40);
+		     root.right.right= new Node (50);
+		     
+		     printpostorder(root);
+		 }
+		 static void printpostorder(Node root) {
+			// TODO Auto-generated method stub
+			if(root==null) {
+				return ;
+			}
+			
+			printpostorder(root.left);
+			printpostorder(root.right);
+			System.out.println(root.key);
+
+			 
+		}
+
+}
+
+}
