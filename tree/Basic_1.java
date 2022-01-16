985d02e11a1f5c0fc0f6d4c47925f9bdadaaa5d9
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-01-16 23:57:00 +0530

    code is added

---
+++ tree/Basic_1.java
@@ -0,0 +1,21 @@
+package tree;
+
+public class Basic_1 {
+
+	static class Node{
+		 int key ;
+		 Node left;
+		 Node right;
+		 Node(int i ){
+			 key=i;
+		 }
+		 public static void main(String [] args) {
+			 Node root = new Node (30);
+			 root.left= new Node (25);
+			 root.left.left= new Node (10);
+			 root.left.right= new Node (20);
+		     root.right= new Node (40);
+		     root.right.right= new Node (50);
+		 }
+	 }
+}
