90c1d4210be548f2a2823384c0ff9bea97ddd405
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-01-25 23:24:23 +0530

    code is added

---
+++ BitwiseOperator/Rightmost different bit.java
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+
+class Main{
+    
+    
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner sc=new Scanner(System.in);
+		
+		//input number of testcases
+		int t=sc.nextInt();
+		int m,n;
+		while(t-->0) {
+		    
+		    
+            Solution obj = new Solution();
+            //input m and n
+		    m = sc.nextInt();
+		    n = sc.nextInt();
+		    System.out.println(obj.posOfRightMostDiffBit(m, n));
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find the first position with different bits.
+    public static int posOfRightMostDiffBit(int m, int n)
+    {
+            
+        // Your code here
+        if(m==0&n==0)
+        return -1;
+        int k =1;
+        while(m!=0||n!=0){
+            if(((m^n)&1)==1){
+                return k;
+            }
+            else{
+                k++;
+                if(m>0)
+                m=m>>1;
+                if(n>0)
+                n=n>>1;
+            }
+        }
+        return -1;
+            
+    }
+}
+
