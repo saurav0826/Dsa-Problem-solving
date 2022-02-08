349473480aaad03174e2b3dd419600681a4b8e18
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-02-09 01:00:28 +0530

    code is added

--- Hashing/First Repeating Element.java
+++ Hashing/First Repeating Element.java
@@ -1,65 +1,56 @@
 // { Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
-import java.util.*;
-import java.lang.*;
 import java.io.*;
-class GFG
-{
-    public static void main(String[] args) throws IOException
-    {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while(T-->0)
-        {
-            int n = Integer.parseInt(br.readLine().trim());
-            String[] S = br.readLine().trim().split(" ");
-            int[] arr = new int[n];
-            for(int i = 0; i < n; i++){
-                arr[i] = Integer.parseInt(S[i]);
+import java.util.*;
+class GFG {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking total count of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // taking total count of elements
+            int n = sc.nextInt();
+
+            // creating a new array of size n
+            int arr[] = new int[n];
+
+            // inserting elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
             }
-            Solution obj = new Solution();
-            obj.swapElements(arr, n);
-            for(int i = 0; i < n; i++){
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
+            Solution ob = new Solution();
+            // calling firstRepeated method
+            // and printing the result
+            System.out.println(ob.firstRepeated(arr, n));
         }
     }
-}
-// } Driver Code Ends
+}// } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
-class Solution
-{
-    public void swapElements(int[] arr, int n)
-    {
-        // Code here
-          int k =arr[0];
-        int l=arr[1];
-        if(n<2)
-        return;
-        else{
-        if(n%2==0){
-        for(int i = 0 ;i<n-2;i++){
-            arr[i]=arr[i+2];
+class Solution {
+    // Function to return the position of the first repeating element.
+    public static int firstRepeated(int[] arr, int n) {
+        // Your code here
+        int ans =0;
+        HashMap<Integer,Integer> s = new HashMap<>();
+        for(int i = 0 ; i < n;i++){
+            if(s.containsKey(arr[i])){
+           s.put(arr[i],s.get(arr[i])+1);
+                }
+                else
+                s.put(arr[i],1);
         }
-        arr[n-2]=k;
-        arr[n-1]=l;
-        return;
+        for(int i = 0 ; i < n;i++){
+            if(s.get(arr[i])>1)
+            return i+1;
         }
-        
-        else{
-            for(int i = 0 ;i<n-2;i++){
-            arr[i]=arr[i+2];
-        }
-        arr[n-2]=l;
-        arr[n-1]=k;
-        return;
-        }
-        
+        return -1;
     }
-}
 }
