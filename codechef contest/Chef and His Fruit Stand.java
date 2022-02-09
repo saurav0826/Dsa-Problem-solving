e6f400662c830a302fa30e2e39518123d2419e42
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-02-09 22:59:31 +0530

    code is added

---
+++ codechef contest/Chef and His Fruit Stand.java
@@ -0,0 +1,20 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc = new Scanner (System.in);
+	int t = sc.nextInt();
+	while(t-->0){
+    int s = sc.nextInt();
+    int u = sc.nextInt();
+    System.out.println(Math.min(s/2,u));
+                }
+	}            
+}
