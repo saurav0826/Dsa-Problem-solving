b6f5df0b770d4ec6798f2266dff361f53ebe63d6
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-02-13 19:53:42 +0530

    code is added

--- Hashing/Subarrays with sum K.java
+++ Hashing/Subarrays with sum K.java
@@ -7,13 +7,19 @@ import java.util.*;
 class GFG{
     public static void main(String args[])throws IOException
     {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(in.readLine());
-        while(t-- > 0){
-            int k = Integer.parseInt(in.readLine());
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int Arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                Arr[i] = Integer.parseInt(input_line[i]);
+            int k = Integer.parseInt(read.readLine());
             
             Solution ob = new Solution();
-            System.out.println(ob.xorCal(k));
+            System.out.println(ob.findSubArraySum(Arr, N, k));
         }
     }
 }// } Driver Code Ends
@@ -21,16 +27,22 @@ class GFG{
 
 //User function Template for Java
 
-class Solution{
-    static int xorCal(int k){
+class Solution
+{
+    static int findSubArraySum(int Arr[], int n, int k)
+    {
         // code here
-        if(k==1)
-        return 2;
-        if((k&(k+1))==0){
-            return k/2;
+        HashMap<Integer , Integer> m= new HashMap<>();
+        m.put(0,1);
+        int ans = 0 ;
+        int cp=0;
+        for(int i =0 ; i < n ;i++){
+            ans+=Arr[i];
+            if(m.containsKey(ans-k))
+            cp+=m.get(ans-k);
+            m.put(ans, m.getOrDefault(ans, 0)+1);
+
         }
-        else
-        return -1;
+        return cp;
     }
-    
 }
