cf46ad1980c2e4cfd1962c2ec756aff24086b29a
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-03-02 23:27:41 +0530

    code is added

--- String/Valid Substring.java
+++ String/Valid Substring.java
@@ -1,59 +1,44 @@
 // { Driver Code Starts
 //Initial Template for Java
-
 import java.io.*;
 import java.util.*;
-import java.util.HashMap; 
-import java.util.HashSet; 
 
-class GFG{
-	public static void main(String args[]) throws IOException { 
-		Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
 
-        while(t > 0){
-       		int rows=sc.nextInt();
-       		int columns=sc.nextInt();
-			
-			int matrix[][]=new int[rows][columns];
-          
-        	for(int i=0; i<rows;i++){            
-            	for(int j=0; j<columns;j++){
-                	matrix[i][j]=sc.nextInt();
-            	}
-         	}
-
-			Solution x = new Solution();
-			if (x.ValidCorner(matrix)) 
-				System.out.println("Yes"); 
-			else
-				System.out.println("No"); 
-			t--;
-		}
-	} 
+            Solution ob = new Solution();
+            System.out.println(ob.findMaxLen(S));
+        }
+    }
 }
-	
-
 
 // } Driver Code Ends
 
 
 //User function Template for Java
-
-public class Solution { 
-	static boolean ValidCorner(int m[][]) 
-	{ 
-	    // Your code goes here
-	    for(int i = 0 ; i< m.length ;i ++){
-	        for(int j = 0; j<m[0].length;j++){
-	            for(int k = i+1;k<m.length;k++){
-	                for(int l=j+1;l<m[0].length;l++){
-	                    if(m[i][j]==1&&m[i][l]==1&&m[k][j]==1&&m[k][l]==1)
-	                    return true;
-	                }
-	            }
-	        }
-	    }
-	    return false;
-	}
+class Solution {
+    static int findMaxLen(String S) {
+        // code here
+     Stack<Integer> stk=new Stack<Integer>();
+       stk.push(-1);
+       int ans = 0;
+       for (int i = 0; i < S.length(); i++) {
+           if (S.charAt(i) == '(') {
+               stk.push(i);
+           } else {
+               stk.pop();
+               if (stk.empty()) {
+                   stk.push(i);
+               } else {
+                   ans = Math.max(ans, i - stk.peek());
+               }
+           }
+       }
+       return ans;
+    }
 }
