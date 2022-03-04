9540f630f1c269de555b3f33765caae6139edb87
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-03-04 23:11:27 +0530

    code is added

--- Hashing/Check if two arrays are equal or not.java
+++ Hashing/Check if two arrays are equal or not.java
@@ -1,59 +1,55 @@
 // { Driver Code Starts
 //Initial Template for Java
 
+
+/*package whatever //do not write package name here */
+
 import java.io.*;
+
 import java.util.*;
-import java.util.HashMap; 
-import java.util.HashSet; 
-
-class GFG{
-	public static void main(String args[]) throws IOException { 
-		Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while(t > 0){
-       		int rows=sc.nextInt();
-       		int columns=sc.nextInt();
-			
-			int matrix[][]=new int[rows][columns];
-          
-        	for(int i=0; i<rows;i++){            
-            	for(int j=0; j<columns;j++){
-                	matrix[i][j]=sc.nextInt();
-            	}
-         	}
-
-			Solution x = new Solution();
-			if (x.ValidCorner(matrix)) 
-				System.out.println("Yes"); 
-			else
-				System.out.println("No"); 
-			t--;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		long t=sc.nextLong();
+		
+		while(t-->0)
+		{
+		  int n=sc.nextInt();
+		  long arr[]=new long[n];
+		  long brr[]=new long[n];
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      arr[i]=sc.nextLong();
+		  }
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      brr[i]=sc.nextLong();
+		  }
+		  Solution ob = new Solution();
+		  System.out.println(ob.check(arr,brr,n)==true?"1":"0");
+		  
+		  
 		}
-	} 
-}
-	
-
-
-// } Driver Code Ends
+	}
+}// } Driver Code Ends
 
 
 //User function Template for Java
 
-public class Solution { 
-	static boolean ValidCorner(int m[][]) 
-	{ 
-	    // Your code goes here
-	    for(int i = 0 ; i< m.length ;i ++){
-	        for(int j = 0; j<m[0].length;j++){
-	            for(int k = i+1;k<m.length;k++){
-	                for(int l=j+1;l<m[0].length;l++){
-	                    if(m[i][j]==1&&m[i][l]==1&&m[k][j]==1&&m[k][l]==1)
-	                    return true;
-	                }
-	            }
-	        }
-	    }
-	    return false;
-	}
+class Solution{
+    //Function to check if two arrays are equal or not.
+    public static boolean check(long A[],long B[],int N)
+    {
+        //Your code here
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for(int i = 0 ;i <N;i++){
+            if(A[i]!=B[i])
+            return false ;
+        }
+        return true ;
+    }
 }
