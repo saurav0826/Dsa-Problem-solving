98d52917bfa0a94258e97d08b6a2b2027575b347
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-03-04 23:58:05 +0530

    code is added

--- Hashing/Find triplets with zero sum.java
+++ Hashing/Find triplets with zero sum.java
@@ -3,7 +3,7 @@
 import java.io.*;
 import java.util.*; 
 class GFG{
-    public static void main(String args[]) throws IOException { 
+    pu
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
         
