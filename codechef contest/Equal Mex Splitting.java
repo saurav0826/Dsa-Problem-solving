faecfd4289677eba3067c6e51cbcf822375f13ae
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-03-05 23:14:14 +0530

    code is added

---
+++ codechef contest/Equal Mex Splitting.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc = new Scanner (System.in);
+	int t = sc.nextInt();
+	while(t-->0){
+             int n = sc.nextInt();
+             int [] a = new int [n] ;
+             for(int i = 0 ; i < n; i++){
+                 a[i]=sc.nextInt();
+             }
+             int ans = 0 ;
+             
+             for(int i = 0 ; i < n;i++){
+                 if(a[i]==0)
+                 ans++;
+             }
+             System.out.println(Math.max(ans,n-ans));
+                }
+	}            
+}
