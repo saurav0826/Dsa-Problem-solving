2aecbb1a5d9e63c62fadd9f6c6ee1e71579165c0
Saurav_Shinde <shindesaurav0826@gmail.com>
2022-03-09 23:54:08 +0530

    code is added

--- queue/IPL 2021 - Match Day 2.java
+++ queue/IPL 2021 - Match Day 2.java
@@ -1,45 +1,68 @@
 // { Driver Code Starts
-//Initial Template for Java
-
-import java.io.*;
 import java.util.*;
-import java.util.HashMap; 
-import java.util.HashSet; 
+import java.io.*;
+import java.lang.*;
 
-class GFG{
+class Main {
+    public static void main(String args[]) {
+        // taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
 
+        // taking total count of testcases
         int t = sc.nextInt();
 
-        while(t > 0){
-       		int rows=sc.nextInt();
-   
-				System.out.println("No"); 
-			t--;
-		}
-	} 
-}
-	
+        while (t-- > 0) {
+            // taking total number of elements
+            int n = sc.nextInt();
+
+            // taking size of subArrays
+            int k = sc.nextInt();
+
+            // Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+
+            // adding all the elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            // Calling the method max_of_subarrays of class solve
+            // and storing the result in an ArrayList
+            ArrayList<Integer> res = new Solution().max_of_subarrays(arr, n, k);
+
+            // printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
 
 
-// } Driver Code Ends
-
-
-//User function Template for Java
-
-public class Solution { 
-	static boolean ValidCorner(int m[][]) 
-	{ 
-	    // Your code goes here
-	    for(int i = 0 ; i< m.length ;i ++){
-	        for(int j = 0; j<m[0].length;j++){
-	            for(int k = i+1;k<m.length;k++){
-	                for(int l=j+1;l<m[0].length;l++){
-	                    if(m[i][j]==1&&m[i][l]==1&&m[k][j]==1&&m[k][l]==1)
-	                    return true;
-	                }
-	            }
-	        }
-	    }
-	    return false;
-	}
+class Solution {
+    static ArrayList<Integer> max_of_subarrays(int arr[], int n, int k) {
+        // Your code here
+          Deque<Integer> q = new ArrayDeque<Integer>();
+          ArrayList<Integer> ans = new ArrayList<Integer>();
+          for(int i = 0; i< k;i++){
+              while(!q.isEmpty()&&arr[i]>=arr[q.peekLast()])
+                 q.removeLast();
+                 q.addLast(i) ;
+              
+              }
+              for(int i = k ;i < n;i++){
+                  ans.add(arr[q.peek()]);
+                 
+                 while(!q.isEmpty()&&q.peek()<=i-k){
+                     q.removeFirst();
+                 }
+                   while(!q.isEmpty()&&arr[i]>=arr[q.peekLast()]){
+                     q.removeLast();
+                 }
+                  q.addLast(i) ;
+              }
+               ans.add(arr[q.peek()]);
+              return ans ;
+              
+    }
 }
